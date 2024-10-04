488aab8c5a140258e7f0f5cf8c7bf15710e66b6e
nick8319 <zhiltsov.nick@gmail.com>
2024-10-04 14:13:17 +0200

    fix: lint

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,8 @@ export const IS_DEV = process.env.NODE_ENV === 'development'
 
 export const GATEWAY_URL_PRODUCTION =
   process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.stage.safe.protofire.io'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.stage.safe.protofire.io'
+export const GATEWAY_URL_STAGING =
+  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.stage.safe.protofire.io'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
