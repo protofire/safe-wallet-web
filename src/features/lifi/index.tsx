3072538da7d4d174a14e28ecad0f5b9a47302dea
msvstj <savastijonok@gmail.com>
2024-10-28 17:52:56 +0200

    feat: send config to widget

--- src/features/lifi/index.tsx
+++ src/features/lifi/index.tsx
@@ -1,5 +1,5 @@
 // import { type MutableRefObject, useEffect, useRef, useState } from 'react'
-import { Box } from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 // import {
 //   type SafeAppData,
 // } from '@safe-global/safe-gateway-typescript-sdk/dist/types/safe-apps'
@@ -8,65 +8,79 @@ import css from './styles.module.css'
 import useSwapConsent from './useSwapConsent'
 import Disclaimer from '@/components/common/Disclaimer'
 import WidgetDisclaimer from '@/components/common/WidgetDisclaimer'
-
-// import { ChainType, WidgetConfig } from '@lifi/widget'
 import AppFrame from '@/components/safe-apps/AppFrame'
 import { getEmptySafeApp } from '@/components/safe-apps/utils'
 import { LIFI_WIDGET_URL } from '@/config/constants'
+import { SafeAppAccessPolicyTypes, SafeAppData, SafeAppFeatures } from '@safe-global/safe-gateway-typescript-sdk'
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from 'react'
+import { useDarkMode } from '@/hooks/useDarkMode'
+import useSafeInfo from '@/hooks/useSafeInfo'
+import { WidgetConfig, ChainType } from '@lifi/widget'
+import useChainId from '@/hooks/useChainId'
 
 const LifiSwapWidget = () => {
-  // const { palette } = useTheme()
-  // const darkMode = useDarkMode()
-  // const chainId = useChainId()
-  // const { safeAddress, safeLoading } = useSafeInfo()
+  const { palette } = useTheme()
+  const darkMode = useDarkMode()
+  const chainId = useChainId()
+  const { safeAddress, safeLoading } = useSafeInfo()
   const { isConsentAccepted, onAccept } = useSwapConsent()
+  const hasSentInitialMessage = useRef(false)
 
-  // const [params, setParams] = useState<WidgetConfig>({
-  //   integrator: "protofire",
-  //   variant: "compact",
-  //   subvariant: "default",
-  //   appearance: darkMode ? 'dark' : 'light',
-  //   toAddress: {
-  //     address: safeAddress,
-  //     chainType: ChainType.EVM,
-  //   },
-  //   fromChain: +chainId,
-  // })
+  const supportedChains = ['42161', '10', '8453', '56', '11235', '59144']
 
-  // useEffect(() => {
-  //   setParams((params) => ({
-  //     ...params,
-  //     appearance: darkMode ? 'dark' : 'light',
-  //     fromChain: +chainId,
-  //   }))
-  // }, [palette, darkMode, chainId])
+  const [params, setParams] = useState<WidgetConfig>({
+    integrator: 'protofire',
+    variant: 'compact',
+    subvariant: 'default',
+    appearance: darkMode ? 'light' : 'dark',
+    toAddress: {
+      address: safeAddress,
+      chainType: ChainType.EVM,
+    },
+    fromChain: +chainId,
+    toChain: +chainId,
+  })
 
-  // const iframeRef: MutableRefObject<HTMLIFrameElement | null> = useRef<HTMLIFrameElement | null>(null)
+  const iframeRef: MutableRefObject<HTMLIFrameElement | null> = useRef<HTMLIFrameElement | null>(null)
 
-  // useEffect(() => {
-  //   const iframeElement = document.querySelector('#lifiWidget iframe')
-  //   if (iframeElement) {
-  //     iframeRef.current = iframeElement as HTMLIFrameElement
-  //   }
-  // }, [isConsentAccepted, safeLoading])
+  useEffect(() => {
+    setParams((params) => ({
+      ...params,
+      appearance: darkMode ? 'light' : 'dark',
+      fromChain: +chainId,
+    }))
+    const window = iframeRef.current?.contentWindow
+    if (window) {
+      window.postMessage(params, '*')
+    }
+  }, [palette, darkMode, chainId])
 
-  // const appData: SafeAppData = useMemo(
-  //   () => ({
-  //     id: 1,
-  //     url: LIFI_WIDGET_URL,
-  //     name: 'LifiSwap',
-  //     iconUrl: darkMode ? './images/common/safe-swap-dark.svg' : './images/common/safe-swap.svg',
-  //     description: 'Safe Apps',
-  //     chainIds: ["42161", "10", "8453", "56", "11235", "59144"],
-  //     accessControl: { type: SafeAppAccessPolicyTypes.NoRestrictions },
-  //     tags: ['safe-apps'],
-  //     features: [SafeAppFeatures.BATCHED_TRANSACTIONS],
-  //     socialProfiles: [],
-  //   }),
-  //   [darkMode],
-  // )
+  useEffect(() => {
+    const iframeElement = document.querySelector('#lifiWidget iframe')
+    if (iframeElement) {
+      iframeRef.current = iframeElement as HTMLIFrameElement
+      const window = iframeRef.current?.contentWindow
+      if (window) {
+        window.postMessage(params, '*')
+      }
+    }
+  }, [isConsentAccepted, safeLoading])
 
-  // useCustomAppCommunicator(iframeRef, appData, chain)
+  const appData: SafeAppData = useMemo(
+    () => ({
+      id: 1,
+      url: LIFI_WIDGET_URL,
+      name: 'LifiSwap',
+      iconUrl: darkMode ? './images/common/safe-swap-dark.svg' : './images/common/safe-swap.svg',
+      description: 'Safe Apps',
+      chainIds: supportedChains,
+      accessControl: { type: SafeAppAccessPolicyTypes.NoRestrictions },
+      tags: ['safe-apps'],
+      features: [SafeAppFeatures.BATCHED_TRANSACTIONS],
+      socialProfiles: [],
+    }),
+    [darkMode],
+  )
 
   if (!isConsentAccepted) {
     return (
@@ -78,22 +92,16 @@ const LifiSwapWidget = () => {
       />
     )
   }
-  const safeAppData = getEmptySafeApp(LIFI_WIDGET_URL)
+
+  const safeAppData = getEmptySafeApp(LIFI_WIDGET_URL, appData)
 
   return (
-    // TODO: wrap lifi into external iframe
     <Box className={css.swapWidget} id="lifiWidget">
       <AppFrame
         appUrl={safeAppData.url}
         allowedFeaturesList="clipboard-read; clipboard-write"
         safeAppFromManifest={safeAppData}
       />
-
-      {/* The preferred scenario below */}
-      {/* {safeLoading ? <WidgetSkeleton config={params!} /> :  <LiFiWidget
-          config={params}
-          integrator="protofire"
-        />}  */}
     </Box>
   )
 }
