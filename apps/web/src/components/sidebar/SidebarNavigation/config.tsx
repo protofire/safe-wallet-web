9c3d235b3958f09aecb952495947f8e5f0b7f873
ajimeno04 <jimenobeltran11@gmail.com>
2025-02-28 19:32:39 +0100

    refactor: Remove Notifications item from sidebar settings

--- apps/web/src/components/sidebar/SidebarNavigation/config.tsx
+++ apps/web/src/components/sidebar/SidebarNavigation/config.tsx
@@ -114,10 +114,10 @@ export const settingsNavItems = [
     label: 'Security',
     href: AppRoutes.settings.security,
   },
-  {
-    label: 'Notifications',
-    href: AppRoutes.settings.notifications,
-  },
+  // {
+  //   label: 'Notifications',
+  //   href: AppRoutes.settings.notifications,
+  // },
   {
     label: 'Modules',
     href: AppRoutes.settings.modules,
